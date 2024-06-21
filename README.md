cfe3cdb6524b2d4688f49e23f0acb2edeea185cd
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-06-21 15:16:04 +0530

    basic express server - get

---
+++ README.md
@@ -0,0 +1,7 @@
+1. git init
+2. create .gitignore,README.md
+3. npm init
+    -important-> index.js, repository
+4. to execute -> node index.js
+     (or)
+   npm run start - paste "start": "node index.js" inside package.json>script>test ku keela
---
+++ index.js
@@ -0,0 +1,15 @@
+//1. import express module
+const exxpress = require('express');
+
+// 2. Create an Express app
+const app = exxpress();
+
+//4. create a route
+app.get('/', (request, response) => {
+    response.send('Hello World!');
+})
+
+//3. run the server
+app.listen(3000, () =>{
+    console.log('Server is running on port http://localhost:3000');
+})
