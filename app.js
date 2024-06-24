901872c361b1efa9e84bbf829822fa9d6af385ed
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-06-24 18:01:18 +0530

    index.js file seperated

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// app.get('/posts', (request, response) => {
+//     response.json(posts);
+// })
+
+// app.post('/posts',(request,response) => {
+//     posts.push(
+//         {
+//             ...request.body,
+//             id: posts.length + 1,
+//         }
+//     );
+//     response.json(
+//         {
+//             ...request.body,
+//             id: posts.length + 1,
+//         }
+//     );
+// })
+
+// app.put('/posts/:id', (request, response) => {
+//     const id = request.params.id;
+//     let UpdatedPost = request.body;
+//     const toUpdatePost = posts.find(post => post.id == id);
+
+//     UpdatedPost={
+//         ...toUpdatePost,
+//         ...UpdatedPost
+//     }
+
+//     posts = posts.map(post => post.id == id ? UpdatedPost : post);
+
+//     response.json(UpdatedPost);
+// })
+
+// app.delete('/posts/:id', (request, response) => {
+
+//   const postDelete = posts.find(post => post.id == request.params.id);
+
+//   if(!postDelete){
+//     response.send({message: 'Post not found'});
+//   }
+
+//   const id = request.params.id;
+//   posts = posts.filter(post => post.id != id);
+//   response.json({ 
+//     message: `Post with id ${id} deleted successfully`
+//   });
+// })
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-
-//1. import express module
-const express = require('express');
 const mongoose = require('mongoose');
-
-// 2. Create an Express app
-const app = express();
-
-//5. middleware
-app.use(express.json());
+const app = require('./app');
 
 console.log('Connecting to MongoDB...');
 
@@ -22,55 +14,5 @@ mongoose.connect(`mongodb+srv://karunakaranrk2001:Harikaran01@cluster0.pfjzhuk.m
 })
 .catch((err)=>console.log('error connecting to Mongodb',err))
 
-//4. create a route
-//response.send-> to send a string
-// app.get('/posts', (request, response) => {
-//     response.json(posts);
-// })
-
-// app.post('/posts',(request,response) => {
-//     posts.push(
-//         {
-//             ...request.body,
-//             id: posts.length + 1,
-//         }
-//     );
-//     response.json(
-//         {
-//             ...request.body,
-//             id: posts.length + 1,
-//         }
-//     );
-// })
-
-// app.put('/posts/:id', (request, response) => {
-//     const id = request.params.id;
-//     let UpdatedPost = request.body;
-//     const toUpdatePost = posts.find(post => post.id == id);
-
-//     UpdatedPost={
-//         ...toUpdatePost,
-//         ...UpdatedPost
-//     }
-
-//     posts = posts.map(post => post.id == id ? UpdatedPost : post);
-
-//     response.json(UpdatedPost);
-// })
-
-// app.delete('/posts/:id', (request, response) => {
-
-//   const postDelete = posts.find(post => post.id == request.params.id);
-
-//   if(!postDelete){
-//     response.send({message: 'Post not found'});
-//   }
-
-//   const id = request.params.id;
-//   posts = posts.filter(post => post.id != id);
-//   response.json({ 
-//     message: `Post with id ${id} deleted successfully`
-//   });
-// })
 
 //3. run the server
