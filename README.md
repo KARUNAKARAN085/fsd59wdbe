b6e37b67a96fb90c5ce17df5b93eee83574bdce5
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-06-24 17:50:17 +0530

    DB connected

--- README.md
+++ README.md
@@ -9,4 +9,45 @@
 6. npm install nodemon --save-dev ->to auto save(don't need to stop)
 7. npm run dev - paste "dev": "nodemon index.js" inside package.json>script>start ku keela
 8. now to execute-> npm run dev
-9. 
+
+
+## steps to setup the application:
+1. Create a new folder.
+2. generate a package.json file by running `npm init -y` command.
+3. Install express.js by running `npm install express mongoose` command.
+4. Create a new entry point file named `index.js`.
+5. In the index file:
+   - Import express
+   - create an express app.
+   - define a basic root route for the app
+   ```javascript
+   app.get("/",(req,res) => {
+    res.send("Welcome to my app");
+   })
+   ```
+   - listen for incoming http request on the specific port
+6. run the application by running `node index.js`
+
+## Enable git version control
+1. Initialize a new git repository by running `git init` command.
+2. Create a `.gitignore` file to ignore unnecessary files.
+3. rename the branch to `main` by running `git branch -M main`.
+4. Add all the files to the staging area by running `git add .` command.
+5. Commit the changes by running `git commit -m "Initial commit"` command.
+6. Create a new repository on GitHub and link it to the local repository by running `git remote add origin <repository-url>` command.
+7. Push the changes to the remote repository by running `git push -u origin main` command.
+
+## create readme
+1. create a new file named `README.md`
+2. add the project description, setup instructions, and any other relevant information in markdown format.
+
+## Enable nodemon for automatic server restarts
+1. Install nodemon by running `npm install nodemon --save-dev` command.
+2. Add a start script in the `package.json` file: 
+    ```json
+      "scripts": {
+        "start": "node index.js",
+        "dev": "nodemon index.js"
+        }
+    ```
+3. Run the application with nodemon by running `npm run dev` command.
--- index.js
+++ index.js
@@ -1,52 +1,7 @@
-const posts = [
-    {
-        "name": "Todd Marks",
-        "username": "Kansas",
-        "email": "Bryana.Legros13@example.net",
-        "address": "Biloxi",
-        "phone": "855-689-2696 x274",
-        "completed": true,
-        "id": "1"
-      },
-      {
-        "name": "Meredith Brown",
-        "username": "systemic",
-        "email": "Jody67@example.com",
-        "address": "North Bethesda",
-        "phone": "(450) 476-3759 x3576",
-        "completed": true,
-        "id": "2"
-      },
-      {
-        "name": "Doug Konopelski",
-        "username": "Cambridgeshire",
-        "email": "Adrienne.Ruecker@example.org",
-        "address": "Aliso Viejo",
-        "phone": "1-852-313-2145 x6281",
-        "completed": true,
-        "id": "3"
-      },
-      {
-        "name": "Ms. Ginger Bins",
-        "username": "Bicycle",
-        "email": "Krista.Morar83@example.org",
-        "address": "Farmington",
-        "phone": "624-428-9096 x4436",
-        "completed": true,
-        "id": "4"
-      },
-      {
-        "name": "Sylvia Murphy IV",
-        "username": "relationships",
-        "email": "Edwardo_Kessler@example.net",
-        "address": "Grand Forks",
-        "phone": "1-204-324-6999 x70563",
-        "completed": true,
-        "id": "5"
-      }
-]
+
 //1. import express module
 const express = require('express');
+const mongoose = require('mongoose');
 
 // 2. Create an Express app
 const app = express();
@@ -54,28 +9,68 @@ const app = express();
 //5. middleware
 app.use(express.json());
 
+console.log('Connecting to MongoDB...');
+
+mongoose.connect(`mongodb+srv://karunakaranrk2001:Harikaran01@cluster0.pfjzhuk.mongodb.net/`)
+.then(()=>{
+  console.log("Connected to MongoDB")
+
+  app.listen(3000, () =>{
+    console.log('Server is running on port http://localhost:3000');
+  })
+
+})
+.catch((err)=>console.log('error connecting to Mongodb',err))
+
 //4. create a route
 //response.send-> to send a string
-app.get('/', (request, response) => {
-    response.json(posts);
-})
+// app.get('/posts', (request, response) => {
+//     response.json(posts);
+// })
 
-app.post('/posts',(request,response) => {
-    posts.push(
-        {
-            ...request.body,
-            id: posts.length + 1,
-        }
-    );
-    response.json(
-        {
-            ...request.body,
-            id: posts.length + 1,
-        }
-    );
-})
+// app.post('/posts',(request,response) => {
+//     posts.push(
+//         {
+//             ...request.body,
+//             id: posts.length + 1,
+//         }
+//     );
+//     response.json(
+//         {
+//             ...request.body,
+//             id: posts.length + 1,
+//         }
+//     );
+// })
+
+// app.put('/posts/:id', (request, response) => {
+//     const id = request.params.id;
+//     let UpdatedPost = request.body;
+//     const toUpdatePost = posts.find(post => post.id == id);
+
+//     UpdatedPost={
+//         ...toUpdatePost,
+//         ...UpdatedPost
+//     }
+
+//     posts = posts.map(post => post.id == id ? UpdatedPost : post);
+
+//     response.json(UpdatedPost);
+// })
+
+// app.delete('/posts/:id', (request, response) => {
+
+//   const postDelete = posts.find(post => post.id == request.params.id);
+
+//   if(!postDelete){
+//     response.send({message: 'Post not found'});
+//   }
+
+//   const id = request.params.id;
+//   posts = posts.filter(post => post.id != id);
+//   response.json({ 
+//     message: `Post with id ${id} deleted successfully`
+//   });
+// })
 
 //3. run the server
-app.listen(3000, () =>{
-    console.log('Server is running on port http://localhost:3000');
-})
