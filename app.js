b92d54a954ac71a5b80b4e276f600eb5c7a17a74
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-06-30 21:31:29 +0530

    Added cors and morgan

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const express = require('express');
 const userRouter = require('./routes/userRoutes');
 const companyRouter = require('./routes/companyRoutes');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
+
+// enable cors
+app.use(cors());
+
+// enable morgan
+app.use(morgan('dev'));
 
 //parse the cookies of the request
 app.use(cookieParser());
